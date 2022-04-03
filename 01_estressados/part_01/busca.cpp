25d1512fc8fdee94b6bba1e5bb68d4b6d5c30de4
Franciel Silveira <francielsilveiraredes@gmail.com>
2022-04-03 19:24:48 -0300

    att

--- 01_estressados/part_01/busca.cpp
+++ 01_estressados/part_01/busca.cpp
@@ -5,16 +5,6 @@
 
 using namespace std;
 
-/*int contar(vector<int> fila, int x) {
-    int contador{0};
-    for (auto& num : fila) {
-        if (num == x) {
-            contador++;
-        }
-    }
-    return contador;
-}*/
-
 int contar(vector<int> fila, int x) {
     int myCount{count(fila.begin(), fila.end(), x)};
     return myCount;
@@ -50,23 +40,9 @@ int procura_valor(vector<int> fila, int x, int inicio) {
 
 int main() 
 {
-    //vector<int> fila {1, 3, 5};
     cout << existe({1, 3, 5}, 3) << '\n';
     cout << contar({1, 1, 4, 1}, 1) << '\n';
     cout << procurar({-1, -50, -1 -99}, -50) << '\n';
     cout << procura_valor({5, 3, -1, -50, -1, -99}, -1, 3) <<  '\n';
-    /*while(true) {
-        string line;
-        getline(cin, line);
-        stringstream ss(line);
-        string cmd;
-        ss >> cmd;
-
-        if (cmd == "exit") {
-            break;
-        } else if ("existe") {
-
-        }
-    }*/
 
 }
